ec0101f21679db23265c612953570494a9208e49
Stephane ERARD <serard06@protonmail.com>
2020-02-01 01:59:22 +0100

    Fixes

--- src/commands/shot.ts
+++ src/commands/shot.ts
@@ -2,26 +2,8 @@ import * as api from "./../lib/Helper"
 
 exports.command = 'shot'
 exports.desc = 'take a qwant shot list !'
-
-export interface ArgvInterface {
-    query: string
-    lite: boolean
-    api: boolean
-    edu: boolean
-    egp: boolean
-    ecosia: boolean
-    bing: boolean
-    lilo: boolean
-    list: Array<string>
-    path: string
-    pages: number
-    userAgent: string
-    resolutions: Array<string>
-}
-
 exports.builder = api.builder;
-
-exports.handler = async function (argv: ArgvInterface) {
+ exports.handler = async function (argv: api.ArgvInterface) {
 
     const request: api.RequestInterface = {
         query: argv.query,
