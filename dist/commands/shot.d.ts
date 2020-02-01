f6e6b4816c1efbbbf2e6e927a40ed928316be998
Stephane ERARD <serard06@protonmail.com>
2020-02-01 01:59:37 +0100

    Fixes

--- dist/commands/shot.d.ts
+++ dist/commands/shot.d.ts
@@ -1,15 +1,1 @@
-export interface ArgvInterface {
-    query: string;
-    lite: boolean;
-    api: boolean;
-    edu: boolean;
-    egp: boolean;
-    ecosia: boolean;
-    bing: boolean;
-    lilo: boolean;
-    list: Array<string>;
-    path: string;
-    pages: number;
-    userAgent: string;
-    resolutions: Array<string>;
-}
+export {};
