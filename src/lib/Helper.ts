dc97bd9081d1e1ddbc5a561b70c4efa40fdfee5e
Stephane ERARD <serard06@protonmail.com>
2020-02-02 00:54:19 +0100

    add config opt

--- src/lib/Helper.ts
+++ src/lib/Helper.ts
@@ -37,26 +37,31 @@ export async function takeAshot(request: RequestInterface): Promise<void> {
         })();
 
 
-        const promises = [];
-        for (var i = 0, j = request.query.length; i < j; i++) {
-            const query = request.query[i];
-            promises.push(
-                api
-                    .takeAshot({
-                        api: config ? config.api : request.api,
-                        bing: config ? config.bing : request.bing,
-                        ecosia: config ? config.ecosia : request.ecosia,
-                        edu: config ? config.edu : request.edu,
-                        egp: config ? config.egp : request.egp,
-                        lilo: config ? config.lilo : request.lilo,
-                        lite: config ? config.lite : request.lite,
-                        pages: config ? config.pages : request.pages,
-                        query: config ? config.query : query,
-                        resolutions: config ? config.resolutions : request.resolutions,
-                        userAgent: config ? config.userAgent : request.userAgent,
-                        basePath: config ? config.userAgent : request.basePath,
-                    }))
-        }
+        const promises = (() => {
+            const _p = [];
+            for (let i = 0, j = request.query.length; i < j; i++) {
+                const query = request.query[i];
+                _p.push(
+                    api
+                        .takeAshot({
+                            api: config ? config.api : request.api,
+                            bing: config ? config.bing : request.bing,
+                            ecosia: config ? config.ecosia : request.ecosia,
+                            edu: config ? config.edu : request.edu,
+                            egp: config ? config.egp : request.egp,
+                            lilo: config ? config.lilo : request.lilo,
+                            lite: config ? config.lite : request.lite,
+                            pages: config ? config.pages : request.pages,
+                            query: config ? config.query : query,
+                            resolutions: config ? config.resolutions : request.resolutions,
+                            userAgent: config ? config.userAgent : request.userAgent,
+                            basePath: config ? config.userAgent : request.basePath,
+                        }))
+            }
+
+            return _p;
+        })();
+
         await Promise.all(promises)
     }
 }
